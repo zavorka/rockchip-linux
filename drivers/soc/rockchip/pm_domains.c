b4a0d08445cc576113e88aa9b70a3441423d7e36
Randy Li <ayaka@soulik.info>
2017-05-09 00:34:17 +0800

    FORWARDPORT: soc: rockchip: power-domain: export idle request
    We need to put the power status of HEVC/RKVDEC IP into IDLE
    unless we can't reset that IP or the SoC would crash down.
    rockchip_pmu_idle_request(dev, true)---> enter idle
    rockchip_pmu_idle_request(dev, false)---> exit idle
    
    Only the video codec drivers of rockchip platform would
    request this patch currently.
    
    I am not sure whether it is necessary to add a new function
    at generic power domain. I want someone give me some advises
    here.
    
    Change-Id: I3a60f24b0d0fdeb32c6b310cdcaa694ceceb9ccd
    Signed-off-by: Caesar Wang <wxt@rock-chips.com>
    Signed-off-by: Jeffy Chen <jeffy.chen@rock-chips.com>
    Signed-off-by: Randy Li <ayaka@soulik.info>

--- drivers/soc/rockchip/pm_domains.c
+++ drivers/soc/rockchip/pm_domains.c
@@ -24,6 +24,8 @@
 #include <dt-bindings/power/rk3368-power.h>
 #include <dt-bindings/power/rk3399-power.h>
 
+#include <linux/rockchip_pmu.h>
+
 struct rockchip_domain_info {
 	int pwr_mask;
 	int status_mask;
@@ -179,6 +181,29 @@ static int rockchip_pmu_set_idle_request(struct rockchip_pm_domain *pd,
 
 	return 0;
 }
+
+int rockchip_pmu_idle_request(struct device *dev, bool idle)
+{
+	struct generic_pm_domain *genpd;
+	struct rockchip_pm_domain *pd;
+	int ret;
+
+	if (IS_ERR_OR_NULL(dev))
+		return -EINVAL;
+
+	if (IS_ERR_OR_NULL(dev->pm_domain))
+		return -EINVAL;
+
+	genpd = pd_to_genpd(dev->pm_domain);
+	pd = to_rockchip_pd(genpd);
+
+	mutex_lock(&pd->pmu->mutex);
+	ret = rockchip_pmu_set_idle_request(pd, idle);
+	mutex_unlock(&pd->pmu->mutex);
+
+	return ret;
+}
+EXPORT_SYMBOL(rockchip_pmu_idle_request);
 
 static int rockchip_pmu_save_qos(struct rockchip_pm_domain *pd)
 {
